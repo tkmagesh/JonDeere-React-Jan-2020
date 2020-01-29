4e3277057ae6d9079f857f0a48c4d1ce88367e85
Magesh Kuppan <tkmagesh77@gmail.com>
2020-01-29 17:21:51 +0530

    Spinner constructor function

---
+++ 01-JavaScript/6-Spinner.js
@@ -0,0 +1,13 @@
+function Spinner(){
+    var counter = 0;
+
+    this.up = function(){
+        return ++counter;
+    }
+
+    this.down = function(){
+        return --counter;
+    }
+}
+
+var spinner = new Spinner();
