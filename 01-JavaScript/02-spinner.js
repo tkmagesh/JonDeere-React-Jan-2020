9f9bd71fde1dbe89fbbb5e544cd20e66bc58cc3e
Magesh Kuppan <tkmagesh77@gmail.com>
2020-01-29 15:17:55 +0530

    Initial commit

---
+++ 01-JavaScript/02-spinner.js
@@ -0,0 +1,57 @@
+
+var spinner = (function (){
+    var counter = 0;
+
+    function up(){
+        return ++counter;
+    }
+
+    function down(){
+        return --counter;
+    }
+
+    return {
+        up : up,
+        down : down
+    }
+})();
+
+function spinnerFactory(){
+    var counter = 0;
+
+    function up(){
+        return ++counter;
+    }
+
+    function down(){
+        return --counter;
+    }
+
+    return {
+        up : up,
+        down : down
+    }
+}
+
+var s1 = spinnerFactory();
+var s2 = spinnerFactory()
+
+//up(), down()
+
+spinner.up() //=> 1
+spinner.up() //=> 2
+spinner.up() //=> 3
+
+spinner.down() //=> 2
+spinner.down() //=> 1
+spinner.down() //=> 0
+spinner.down() //=> -1
+
+//The following should not be able to be done:
+
+spinner.counter = 10000
+spinner.up()
+
+
+counter = 10000
+spinner.up()
