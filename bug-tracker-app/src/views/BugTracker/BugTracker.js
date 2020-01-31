8e5df9c4ce34458061096caa2df66f0879d509dd
Magesh Kuppan <tkmagesh77@gmail.com>
2020-01-31 16:51:49 +0530

    using useEffect hook

--- bug-tracker-app/src/views/BugTracker/BugTracker.js
+++ bug-tracker-app/src/views/BugTracker/BugTracker.js
@@ -1,32 +1,26 @@
-import React, { Component, useState } from 'react';
+import React , { useEffect } from 'react';
 import './BugTracker.css';
 import useBugs from './useBugs';
-
+import BugEdit from './BugEdit';
 
 
 const BugTracker = () => {
-    
-    const [newBugName, setNewBugName] = useState('');
-    const [bugs, loadBugs, addNew ] = useBugs();
 
+    const [bugs, loadBugs, addNew ] = useBugs();
     const bugItems = bugs.map(bug => (
         <li key={bug.id}>
             <span>{bug.name}</span>
             <div id="datetime">{bug.createdAt}</div>
         </li>
     ) );
-
+    
+    useEffect(() => loadBugs())
     return (
         <div>
             <h3>Bug Tracker</h3>
             <hr />
-            <input type="button" value="Load" onClick={loadBugs} />
             <div>{bugs.length}</div>
-            <section className="edit">
-                <label htmlFor="">Bug Name :</label>
-                <input type="text" onChange={evt => setNewBugName(evt.target.value)}  />
-                <input type="button" value="Add new" onClick={() => addNew(newBugName)} />
-            </section>
+            <BugEdit addNew={addNew} />
             <ol>
                 {bugItems}
             </ol>
