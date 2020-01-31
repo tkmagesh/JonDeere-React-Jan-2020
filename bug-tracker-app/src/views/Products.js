68d6fbc5106ce7f2cb80cead393f7d0fcfafb8d6
Magesh Kuppan <tkmagesh77@gmail.com>
2020-01-31 14:30:15 +0530

    implementing 'remove product' functionality

--- bug-tracker-app/src/views/Products.js
+++ bug-tracker-app/src/views/Products.js
@@ -26,20 +26,29 @@ import React, {useState, useReducer} from 'react';
         )
     }
 } */
+const productReducer = (prevState, action) => {
+    //action = { type : 'ADD', payload : newProductName }
+    if (action.type === 'ADD')
+        return [...prevState, action.payload];
+    if (action.type === 'REMOVE')
+        return prevState.filter(productName => productName !== action.payload);
+};
 
 const Products = () => {
     const [newProductName, setNewProductName] = useState('');
-    const [productNames, addNewProductDispatcher] = useReducer((prevState, action) => {
-        //action = { type : 'ADD', payload : newProductName }
-        if (action.type === 'ADD')
-            return [...prevState, action.payload]
-    }, []);
-    const productItems = productNames.map((productName, index) => (<li key={index}>{productName}</li>));
+
+    const [productNames, productDispatcher] = useReducer(productReducer, []);
+    const productItems = productNames.map((productName, index) => (
+        <li key={index}>
+            <span>{productName}</span>
+            <input type="button" value="Remove" onClick={ () => productDispatcher({ type : 'REMOVE', payload : productName})} />
+        </li>
+    ));
     return (
         <div>
             <label>Product Name :</label>
             <input type="text" onChange={evt => setNewProductName(evt.target.value)} />
-            <input type="button" value="Add New" onClick={() => addNewProductDispatcher({ type: 'ADD', payload: newProductName})} />
+            <input type="button" value="Add New" onClick={() => productDispatcher({ type: 'ADD', payload: newProductName})} />
             <ol>
                 {productItems}
             </ol>
